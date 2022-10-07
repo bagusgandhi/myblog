2435b2366e5e2ad8d4a68938abdfb5817283a7f2
bagusgandhi <bagusgandhi4@gmail.com>
2022-10-07 20:29:24 +0700

    fix problem share button

--- pages/posts/[slug].js
+++ pages/posts/[slug].js
@@ -4,6 +4,10 @@ import {
     FacebookIcon,
     TwitterShareButton,
     TwitterIcon,
+    LinkedinShareButton,
+    LinkedinIcon,
+    WhatsappShareButton,
+    WhatsappIcon,
   } from 'next-share';
 import { useEffect } from 'react';
 import { getPostBySlug, getAllPosts } from "../../lib/ghost";
@@ -21,15 +25,17 @@ export async function getStaticPaths(){
 export async function getStaticProps({ params }){
     const { slug } = params;
     const data = await getPostBySlug(slug);
+    const url = process.env.APP_URL;
     return { props: { 
         data,
         slug,
-        type: 'article' 
+        type: 'article',
+        url 
     } }
 }
 
 
-export default function Post({ data, slug }){
+export default function Post({ data, slug, url }){
 
     useEffect(() => {
         var disqus_config = function () {
@@ -53,15 +59,24 @@ export default function Post({ data, slug }){
         <article>
             <div className='pb-8 sm:pb-12'>
                 <Content data={data} />
-                <div className='flex gap-2'>
+                <div className='flex gap-4 items-center justify-center p-8'>
+                <small className='text-secondary dark:text-yellow text-center'>Share </small>
                     <FacebookShareButton
-                        url={`${process.env.APP_URL}/posts/${slug}`} >
+                        url={`${url}/posts/${slug}`} >
                         <FacebookIcon size={32} round />
                     </FacebookShareButton>
                     <TwitterShareButton
-                        url={`${process.env.APP_URL}/posts/${slug}`} >
+                        url={`${url}/posts/${slug}`} >
                         <TwitterIcon size={32} round />
                     </TwitterShareButton>
+                    <LinkedinShareButton
+                        url={`${url}/posts/${slug}`} >
+                        <LinkedinIcon size={32} round />
+                    </LinkedinShareButton>
+                    <WhatsappShareButton
+                        url={`${url}/posts/${slug}`} >
+                        <WhatsappIcon size={32} round />
+                    </WhatsappShareButton>
                 </div>
             </div>
             <div className='p-8 bg-white rounded-xl'>
